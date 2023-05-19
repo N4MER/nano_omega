7e009741cda7b79ac2f026e8700645d319e66714
N4MER <yangzhaoxiang@seznam.cz>
2023-05-19 12:44:32 +0200

    combat

---
+++ src/Character.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Character {
+    private int hp = 100;
+    private int attack = 10;
+    private int mana = 100;
+    private ArrayList<String> skills = new ArrayList<>();
+
+}
